61a55b80307b64c3dd274ba72623a405e34cd88c
godxiaocui <godxiaocui@gmail.com>
2023-03-02 20:02:04 +0800

    master-hot

--- mybatis_parameter/src/test/java/com/czh/mybatis/test/ParameterTest.java
+++ mybatis_parameter/src/test/java/com/czh/mybatis/test/ParameterTest.java
@@ -38,7 +38,7 @@ public class ParameterTest {
         SqlSession sqlSessdion = SqlSessionUtils.getSqlSessdion();
         UserMapper mapper = sqlSessdion.getMapper(UserMapper.class);
         User vbc = (User) mapper.CheckLoginParam("ybc","123");
-        System.out.println(vbc);
+        System.out.println(vbc+"master");
     }
     @Test
     public void CheckLoginParamAgain() throws IOException {
