b71e43096e87d3ff58dccbb73dab3d4d6509f4c5
godxiaocui <godxiaocui@gmail.com>
2023-03-11 11:35:51 +0800

    first commit

--- mybatis- resultmap/src/main/java/com/czh/mybatis/mapper/EmpMapper.java
+++ mybatis- resultmap/src/main/java/com/czh/mybatis/mapper/EmpMapper.java
@@ -1,4 +1,9 @@
 package com.czh.mybatis.mapper;
 
+import com.czh.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 public interface EmpMapper {
+
+    Emp getEmpByid (@Param("empId") Integer id);
 }
